3704ea791ad071b8bd40f894396d149becfdc460
justdvnsh <justdvnsh2208@gmail.com>
2020-04-10 15:55:01 +0530

    added drawer and bottom bar main actiity

--- app/src/main/java/com/divyansh/blackcoffer/ui/MainActivity.java
+++ app/src/main/java/com/divyansh/blackcoffer/ui/MainActivity.java
@@ -1,16 +1,74 @@
 package com.divyansh.blackcoffer.ui;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.divyansh.blackcoffer.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitleColor(getResources().getColor(R.color.colorPrimaryGreyDark));
+        setTitle("#tags\nGolf Course Road, Delhi");
+
+        dl = findViewById(R.id.activity_main_drawer);
+        t = new ActionBarDrawerToggle(this, dl, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = findViewById(R.id.nv);
+        nv.bringToFront();
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                switch(id) {
+                    case R.id.nav_dashboard:
+                        break;
+                    case R.id.create_events:
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.notif) {
+            // do something
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
